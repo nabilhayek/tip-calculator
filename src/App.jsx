f91baf5d72d57037dedef7a92599043065b7870a
Nabil <hayeknabil214@gmail.com>
2023-06-28 15:02:34 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='container'>
+      <div className='box'>
+        <div className='box-inputs'>
+          <div>
+            <p>Bill</p>
+            <input type='number' />
+          </div>
+          <div>
+            <p>Select Tip %</p>
+            <button>5%</button>
+            <button>10%</button>
+            <button>15%</button>
+            <button>25%</button>
+            <button>50%</button>
+            <input type='number' placeholder='Custom' />
+          </div>
+          <div>
+            <p>Number of People</p>
+            <input type='number' />
+          </div>
+        </div>
+        <div className='box-amount'></div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
